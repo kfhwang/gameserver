e7c00f1a64d688cacd548b665c936ad95031dae2
kfhwang <kfhwang@nutc.edu.tw>
2025-05-20 10:21:10 +0800

    20250520

--- server.js
+++ server.js
@@ -6,12 +6,19 @@
 var express = require("express");
 var server = express();
 
+var bodyParser = require("body-parser");
+
+
 server.use(express.static(__dirname+"/public"));
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.json());
 
 // server.get("/", function (req, res) { //web root
 //     res.send("Hello, World!"); //回傳固定內容
 // });
-// server.get("/score", function (req, res) { //other pages
-//     res.send("req /md");
-// });
+ server.get("/score", function (req, res) { //other pages
+    //save db
+     res.send("req /md");
+ });
+ 
 server.listen(80);
